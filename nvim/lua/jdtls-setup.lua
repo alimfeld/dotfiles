d410100825f96930327f82f891472d959c8c4b51
alimfeld <aimfeld@gmx.ch>
2021-03-17 14:02:10 +0100

    Add additional root markers

--- nvim/lua/jdtls-setup.lua
+++ nvim/lua/jdtls-setup.lua
@@ -45,7 +45,7 @@ function M.setup()
 		require('jdtls.setup').add_commands()
 	end
 
-	local root_markers = {'.git'}
+	local root_markers = {'.git', 'mvnw', 'gradlew'}
 	local root_dir = require('jdtls.setup').find_root(root_markers)
 	local home = os.getenv('HOME')
 	local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
