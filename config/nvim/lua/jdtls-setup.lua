df7342ccb5b21489a7e861780dfbefeb76fb0e4f
alimfeld <aimfeld@gmx.ch>
2021-04-24 09:19:33 +0200

    Change jdtls workspace location

--- config/nvim/lua/jdtls-setup.lua
+++ config/nvim/lua/jdtls-setup.lua
@@ -50,7 +50,7 @@ function M.setup()
 	local root_markers = {'.git', 'mvnw', 'gradlew'}
 	local root_dir = require('jdtls.setup').find_root(root_markers)
 	local home = os.getenv('HOME')
-	local workspace_folder = home .. "/.local/share/eclipse/jdt-language-server/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+	local workspace_folder = home .. "/.jdtls/workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
 	local config = {}
 	config.cmd = {'jdtls', workspace_folder}
