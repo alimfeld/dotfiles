e2af2f05f2e44ef2039cfa6034221ec2ef91faa6
alimfeld <aimfeld@gmx.ch>
2021-12-26 12:42:07 +0100

    Extend mappings

--- nvim/.config/nvim/lua/setup/mappings.lua
+++ nvim/.config/nvim/lua/setup/mappings.lua
@@ -1,6 +1,18 @@
 local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
 -- leader
-vim.g.mapleader = ' '
-map('n', '<Space>', '<Nop>', {noremap = true})
+vim.g.mapleader = " "
+map("n", "<Space>", "<Nop>", { noremap = true })
 
+-- navigate buffers
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- move lines
+map("n", "<A-j>", ":m .+1<CR>==", opts)
+map("n", "<A-k>", ":m .-2<CR>==", opts)
+map("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+map("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
