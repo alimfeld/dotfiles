b18992d7d0827a666f57db8bef6374405fb90811
Alexander Imfeld <alexander.imfeld@netcetera.com>
2022-01-22 11:18:21 +0100

    Stop autoformatting on save

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -7,9 +7,4 @@ null_ls.setup({
 		builtins.formatting.stylua,
 		builtins.formatting.prettier,
 	},
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-		end
-	end,
 })
