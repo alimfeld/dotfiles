00048d47ae6198a5e9a5471624a407214b6e6c8d
Alexander Imfeld <alexander.imfeld@netcetera.com>
2022-09-13 11:19:21 +0200

    Add sdkman / aswume support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,12 @@ if status is-interactive
     fish_vi_key_bindings
     set fish_greeting
 end
+
+
+if test -d $HOME/.sdkman
+    function sdk
+        bash -c "source '$HOME/.sdkman/bin/sdkman-init.sh'; sdk $argv[1..]"
+    end
+
+    fish_add_path (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
+end
