ddbf70521c67b175bcec56a697589c9b465f317c
Alexander Imfeld <alexander.imfeld@netcetera.com>
2022-11-17 20:25:29 +0100

    jdtls setup

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -1,6 +1,1 @@
-local home = os.getenv('HOME')
-local config = {
-    cmd = {home .. '/.local/share/nvim/mason/packages/jdtls/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-}
-require('jdtls').start_or_attach(config)
+require("config.lsp").setup_java()
--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,39 +1,39 @@
 local M = {}
 
-function M.setup()
-  local on_attach = function(_, bufnr)
-    local nmap = function(keys, func, desc)
-      if desc then
-        desc = "LSP: " .. desc
-      end
-
-      vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+local on_attach = function(_, bufnr)
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
     end
 
-    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-    nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-    -- Lesser used LSP functionality
-    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-    nmap("<leader>wl", function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, "[W]orkspace [L]ist Folders")
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
-  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+  nmap("<leader>k", vim.lsp.buf.signature_help, "Signature Documentation")
+
+  -- Lesser used LSP functionality
+  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  nmap("<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, "[W]orkspace [L]ist Folders")
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+function M.setup()
   require("mason").setup()
   require("mason-null-ls").setup({
     automatic_setup = true,
@@ -66,6 +66,37 @@ function M.setup()
       })
     end,
   })
+end
+
+local jdtls_configured = false
+local jdtls_cmd
+
+function M.setup_java()
+  if not jdtls_configured then
+    -- we rely on jdtls being installed via Mason
+    local home = os.getenv("HOME")
+    local base = home .. "/.local/share/nvim/mason/packages/jdtls"
+    if vim.fn.isdirectory(base) then
+      -- conveniently Mason also downloads lombok
+      local lombok = base .. "/lombok.jar"
+      jdtls_cmd = {
+        base .. "/bin/jdtls",
+        "--jvm-arg=-javaagent:" .. lombok,
+      }
+    end
+    -- do this configuration just once
+    jdtls_configured = true
+  end
+
+  if jdtls_cmd then
+    local config = {
+      cmd = jdtls_cmd,
+      on_attach = on_attach,
+      capabilities = capabilities,
+      root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" }, { upward = true })[1]),
+    }
+    require("jdtls").start_or_attach(config)
+  end
 end
 
 return M
