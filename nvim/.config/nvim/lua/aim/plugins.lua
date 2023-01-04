e2dae8c835366be0f2bb615461618503d4d68585
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-01-04 21:20:17 +0100

    dap keymaps

--- nvim/.config/nvim/lua/aim/plugins.lua
+++ nvim/.config/nvim/lua/aim/plugins.lua
@@ -72,22 +72,11 @@ vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Find buff
 vim.keymap.set("n", "<leader>fp", telescope_builtin.live_grep, { desc = "Find pattern" })
 vim.keymap.set("n", "<leader>fd", telescope_builtin.diagnostics, { desc = "Find diagnostics" })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local dap = require("dap")
-    vim.keymap.set("n", "<C-p>", dap.toggle_breakpoint, { desc = "Toggle breakpoint", buffer = 0 })
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if (client.name == 'jdtls') then
-      local jdtls = require("jdtls")
-      vim.keymap.set("n", "<leader>ji", jdtls.organize_imports, { desc = "Organize imports", buffer = 0 })
-      vim.keymap.set("n", "<leader>df", jdtls.test_class, { desc = "Test class", buffer = 0 })
-      vim.keymap.set("n", "<leader>dn", jdtls.test_nearest_method, { desc = "Test nearest method", buffer = 0 })
-    end
-  end,
-})
-
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.set_preferences({
+  set_lsp_keymaps = { omit = { "<C-k>" } }
+})
 lsp.nvim_workspace()
 lsp.build_options("jdtls")
 lsp.setup()
@@ -97,3 +86,25 @@ require("mason-null-ls").setup_handlers({})
 require("null-ls").setup()
 require("mason-nvim-dap").setup({ automatic_setup = true })
 require("mason-nvim-dap").setup_handlers({})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local dap = require("dap")
+    vim.keymap.set("n", "<F5>", dap.continue, { desc = "Continue", buffer = 0 })
+    vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step over", buffer = 0 })
+    vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into", buffer = 0 })
+    vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out", buffer = 0 })
+    vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "Toggle breakpoint", buffer = 0 })
+    vim.keymap.set("n", "<Leader>B", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
+      { desc = "Set conditional breakpoint", buffer = 0 })
+    vim.keymap.set("n", "<Leader>dr", dap.repl.open, { desc = "Open a REPL", buffer = 0 })
+    vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "Run last", buffer = 0 })
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if (client.name == 'jdtls') then
+      local jdtls = require("jdtls")
+      vim.keymap.set("n", "<leader>ji", jdtls.organize_imports, { desc = "Organize imports", buffer = 0 })
+      vim.keymap.set("n", "<leader>df", jdtls.test_class, { desc = "Test class", buffer = 0 })
+      vim.keymap.set("n", "<leader>dn", jdtls.test_nearest_method, { desc = "Test nearest method", buffer = 0 })
+    end
+  end,
+})
