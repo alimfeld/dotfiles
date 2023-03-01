df30185f6a8f55e78eab99ff21854f6af2b830e3
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-03-01 13:55:53 +0100

    Add mini.pairs and mini.surrounding

--- nvim/.config/nvim/lua/plugins/edit.lua
+++ nvim/.config/nvim/lua/plugins/edit.lua
@@ -4,4 +4,18 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     config = true,
   },
+  {
+    "echasnovski/mini.surround",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function(_, opts)
+      require("mini.surround").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
+  },
 }
