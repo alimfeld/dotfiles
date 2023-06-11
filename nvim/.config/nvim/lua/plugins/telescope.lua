6a1cc84bde22e4d780cef0f6453cd8c81df412a5
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-06-11 20:28:28 +0200

    feat: add telescope-live-grep-args

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,17 @@ return {
     "nvim-telescope/telescope.nvim",
     event = "VeryLazy",
     branch = "0.1.x",
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim"
+    },
     config = function()
-      local telescope = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Find file" })
-      vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Find buffer" })
-      vim.keymap.set("n", "<leader>fp", telescope.live_grep, { desc = "Find pattern" })
-      vim.keymap.set("n", "<leader>fd", telescope.diagnostics, { desc = "Find diagnostics" })
+      local telescope = require("telescope")
+      telescope.load_extension("live_grep_args")
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find file" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffer" })
+      vim.keymap.set("n", "<leader>fp", telescope.extensions.live_grep_args.live_grep_args, { desc = "Find pattern" })
+      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Find diagnostics" })
     end,
   },
 }
