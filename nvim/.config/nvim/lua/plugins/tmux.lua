854835afb6ef602988e4d17a470c2ed0d071a4db
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-06-24 12:46:05 +0200

    Switch to LazyVim

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -1,3 +1,18 @@
+-- see https://github.com/LazyVim/LazyVim/issues/544
 return {
-  { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
+  {
+    "numToStr/Navigator.nvim",
+    lazy = true,
+    config = function()
+      require("Navigator").setup({
+        auto_save = "current",
+      })
+    end,
+    keys = {
+      { "<C-h>", "<CMD>NavigatorLeft<CR>" },
+      { "<C-l>", "<CMD>NavigatorRight<CR>" },
+      { "<C-k>", "<CMD>NavigatorUp<CR>" },
+      { "<C-j>", "<CMD>NavigatorDown<CR>" },
+    },
+  },
 }
