d3bbf2c24e7e80ea9a8ebdc23fe60e2321006fc0
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-06-27 18:40:23 +0200

    style: remove nesting

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,10 +1,8 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = "cd app && yarn install",
-    keys = {
-      { "<leader>m", ":MarkdownPreview<CR>", { desc = "Toggle tree" } },
-    },
+  "iamcco/markdown-preview.nvim",
+  ft = "markdown",
+  build = "cd app && yarn install",
+  keys = {
+    { "<leader>m", ":MarkdownPreview<CR>", { desc = "Toggle tree" } },
   },
 }
