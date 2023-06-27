0ae00355218c6ec41f4d80d82428cca783da56c8
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-06-27 17:09:14 +0200

    feat: disable some plugins

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "ggandor/flit.nvim", enabled = false },
+  { "ggandor/leap.nvim", enabled = false },
+
+  { "RRethy/vim-illuminate", enabled = false },
+
+  { "folke/neoconf.nvim", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
+}
