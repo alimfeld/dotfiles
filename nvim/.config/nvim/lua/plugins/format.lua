211bdf84b8a353f0921167706e723bd59b783490
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-07-21 16:02:43 +0200

    feat: add neoformat and update plugins

---
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -0,0 +1,8 @@
+return {
+  "sbdchd/neoformat",
+  cmd = "Neoformat",
+  keys = {
+    { "<leader>cf", ":Neoformat<CR>", desc = "Format" },
+  },
+  version = false,
+}
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,9 +14,9 @@ return {
           -- <c>ode
           vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = 0, desc = "Code action" })
           vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0, desc = "Rename" })
-          vim.keymap.set("n", "<leader>cf", function()
-            vim.lsp.buf.format { async = true }
-          end, { buffer = 0, desc = "Format" })
+          -- vim.keymap.set("n", "<leader>cf", function()
+          --   vim.lsp.buf.format { async = true }
+          -- end, { buffer = 0, desc = "Format" })
         end,
       })
     end,
