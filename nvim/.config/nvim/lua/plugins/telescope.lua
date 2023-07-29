82fd6785beeb03b12b21c00107a96698f8bc5600
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-07-29 15:00:07 +0200

    feat: tune telescope keybindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -14,14 +14,14 @@ return {
       -- <f>ind (file)
       vim.keymap.set("n", "<leader><space>", builtin.find_files, { desc = "Files" })
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Files" })
+      vim.keymap.set("n", "<leader>fh", function()
+        builtin.find_files({ hidden = true })
+      end, { desc = "Hidden files" })
       vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-      -- <b>uffer
-      vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "Buffers" })
       -- <s>earch
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Diagnostics" })
-      vim.keymap.set("n", "<leader>sg", telescope.extensions.live_grep_args.live_grep_args,
-        { desc = "Grep" })
+      vim.keymap.set("n", "<leader>sg", telescope.extensions.live_grep_args.live_grep_args, { desc = "Grep" })
       vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "Commands" })
       vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Help tags" })
       vim.keymap.set("n", "<leader>sq", builtin.quickfix, { desc = "Quickfix" })
