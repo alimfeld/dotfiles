aca760265b937cb886995e5c6fcc6c328cca3286
Alexander Imfeld <alexander.imfeld@netcetera.com>
2023-11-28 19:36:44 +0100

    style: remove disabled plugins

--- nvim/.config/nvim/lua/plugins/disabled/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  build = "cd app && yarn install",
-  ft = "markdown",
-  keys = {
-    { "<leader>m", ":MarkdownPreview<CR>", desc = "Preview markdown" },
-  },
-}
