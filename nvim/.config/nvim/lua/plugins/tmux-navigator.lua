e0a2c227c756b4eb4470183fea702654526d15c4
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-03-08 17:45:30 +0100

    feat: describe tmux navigator key bindings

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,11 +1,11 @@
 return {
   "christoomey/vim-tmux-navigator",
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Jump to the window on the left" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Jump to the window below" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Jump to the window above" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Jump to the window on the right" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Jump to the previous window" },
   },
   config = function()
     vim.g.tmux_navigator_preserve_zoom = 1
