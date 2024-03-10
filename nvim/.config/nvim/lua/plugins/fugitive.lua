85de570be78c31d52eecae9fc2a3e2c997c4f829
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-03-10 19:29:09 +0100

    feat: add fugitive keymaps

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,9 @@
 return {
   "tpope/vim-fugitive",
+  keys = {
+    { "<leader>gg", "<cmd>Git<cr>", desc = "Git" },
+    { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Diff" },
+    { "<leader>gb", "<cmd>Git blame<cr>", desc = "Blame" },
+    { "<leader>gr", "<cmd>0Gclog<cr>", desc = "Log" },
+  },
 }
