cbb260e62259bcbca6acd774c54e1e92a621bdf3
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-03-11 21:36:50 +0100

    fix: git mappings

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,9 +2,9 @@ return {
   "tpope/vim-fugitive",
   keys = {
     { "<leader>gg", "<cmd>Git<cr>", desc = "Git" },
-    { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Diff" },
+    { "<leader>gd", "<cmd>Gvdiffsplit!<cr>", desc = "Diff" },
     { "<leader>gb", "<cmd>Git blame<cr>", desc = "Blame" },
-    { "<leader>gr", "<cmd>0Gclog<cr>", desc = "Log" },
+    { "<leader>gl", "<cmd>0Gclog<cr>", desc = "Log" },
     { "<leader>gp", "<cmd>Git pull<cr>", desc = "Pull" },
     { "<leader>gP", "<cmd>Git push<cr>", desc = "Push" },
   },
