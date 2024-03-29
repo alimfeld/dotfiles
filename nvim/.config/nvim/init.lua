52b40d29d23e06ebfdccb31177d472bfe377c721
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-03-29 15:09:16 +0100

    feat: vim tab/shift options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,10 @@ vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
