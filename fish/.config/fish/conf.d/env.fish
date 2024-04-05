1a748f1b04634ad6d2ec1f81ea5a863ce2d61199
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-04-05 18:49:55 +0200

    feat: set XDG env variables
    otherwise k9s won't find its configuration

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -5,3 +5,9 @@ fish_add_path ~/.local/scripts
 # Default programs
 set -x EDITOR nvim
 set -x VISUAL $EDITOR
+
+# XDG
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_STATE_HOME $HOME/.local/state
