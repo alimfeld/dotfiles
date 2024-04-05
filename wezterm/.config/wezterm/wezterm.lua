5b84df06979ea9f713e5dd5903b81ad1902f57a0
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-04-05 20:15:05 +0200

    feat: back to 16 color gruvbox

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ config.default_prog = { shell, "-l" }
 config.set_environment_variables = {
 	SHELL = shell,
 }
-config.color_scheme = "GruvboxDarkHard"
+config.color_scheme = "Gruvbox dark, medium (base16)"
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
