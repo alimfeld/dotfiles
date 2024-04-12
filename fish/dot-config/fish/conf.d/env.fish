186bbe1331517afa29f71901c0ab604cca32eec7
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-04-12 16:41:12 +0200

    style: remove LSCOLORS

--- fish/dot-config/fish/conf.d/env.fish
+++ fish/dot-config/fish/conf.d/env.fish
@@ -13,7 +13,6 @@ set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_STATE_HOME $HOME/.local/state
 
 # Colors
-set -x LSCOLORS ExFxcxdxbxegedabagacad
 set -x FZF_DEFAULT_OPTS "\
 --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
