59624a1d8f0b8d33082c526288fa47c825fcc536
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-04-13 09:02:23 +0200

    feat: add git key bindings

--- nvim/dot-config/nvim/lua/plugins/diffview.lua
+++ nvim/dot-config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,9 @@
 return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+  keys = {
+    { "<leader>gh", "<cmd>DiffviewFileHistory %<CR>", desc = "Git File History" },
+    { "<leader>gi", "<cmd>DiffviewOpen<CR>", desc = "Git Diff Index" },
+  },
   config = true,
 }
--- nvim/dot-config/nvim/lua/plugins/fugitive.lua
+++ nvim/dot-config/nvim/lua/plugins/fugitive.lua
@@ -1,4 +1,8 @@
 return {
   "tpope/vim-fugitive",
   event = "VeryLazy",
+  keys = {
+    { "<leader>gs", "<cmd>Git<CR>", desc = "Git Status" },
+    { "<leader>gd", "<cmd>Gvdiffsplit<CR>", desc = "Git Diff" },
+  },
 }
