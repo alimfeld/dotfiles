c8ac63937ba5ea5418068910481a30d18a09c202
Alexander Imfeld <alexander.imfeld@netcetera.com>
2024-04-23 18:21:58 +0200

    feat: ensure homebrew is first in path

--- fish/dot-config/fish/conf.d/brew.fish
+++ fish/dot-config/fish/conf.d/brew.fish
@@ -1,4 +1,5 @@
 if test -d /opt/homebrew
+    fish_add_path -gPm /opt/homebrew/bin
     eval (/opt/homebrew/bin/brew shellenv)
 else if test -d /home/linuxbrew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
