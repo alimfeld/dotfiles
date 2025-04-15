c69a4b74ca2b1de37beb1c1f1a1f8ebf5534d973
Alexander Imfeld <alexander.imfeld@netcetera.com>
2025-04-15 09:40:06 +0200

    feat: re-introduce blink

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -48,14 +48,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end
-  end,
-})
 
 -- Bootstrap lazy.nvim (see https://lazy.folke.io/installation)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
