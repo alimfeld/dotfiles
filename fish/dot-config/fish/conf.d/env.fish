5901b8d138a4c173f947f57d4917aaa6fcbc55a7
Alexander Imfeld <alexander.imfeld@netcetera.com>
2025-08-05 14:21:31 +0200

    feat: ansi colors

--- fish/dot-config/fish/conf.d/env.fish
+++ fish/dot-config/fish/conf.d/env.fish
@@ -12,9 +12,3 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_STATE_HOME $HOME/.local/state
-
-# Colors
-set -x FZF_DEFAULT_OPTS "\
---color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
---color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
---color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
