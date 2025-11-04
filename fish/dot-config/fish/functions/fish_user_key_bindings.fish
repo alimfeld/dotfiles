04fc626e13760d30f6e98c563424af43f6630dc1
Alexander Imfeld <alexander.imfeld@netcetera.com>
2025-11-04 09:13:15 +0100

    feat: fix keybinding

--- fish/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
-    bind -M insert -k nul tmux-sessionizer
+    bind -M insert ctrl-space tmux-sessionizer
     bind -M visual -m default y 'fish_clipboard_copy; commandline -f end-selection repaint-mode'
     bind yy fish_clipboard_copy
     bind p fish_clipboard_paste
