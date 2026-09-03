ced844490cab73715563e7797f672f862eccdffb
Alexander Imfeld <Alexander.Imfeld@netcetera.com>
2026-09-03 17:28:56 +0200

    Use JetBrains Mono Nerd Font and align Ghostty theme with Neovim contrast
    - Ghostty/WezTerm: set font-family to JetBrainsMono Nerd Font Mono
    - Ghostty: switch theme from Gruvbox Dark to Gruvbox Dark Hard to match
      Neovim's retrobox contrast ratio (~12:1 vs ~10.7:1)

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -10,6 +10,7 @@ config.set_environment_variables = {
 }
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 14
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
 
 config.color_scheme = 'NvimDark'
 
