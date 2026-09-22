fb21ee546e6502d56a7529efb79fbd85e11541b8
Alexander Imfeld <Alexander.Imfeld@netcetera.com>
2026-09-22 10:16:03 +0200

    feat: add diffview

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highl
 
 -- Diagnostics
 vim.diagnostic.config({ virtual_text = true })
-vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Open [d]iagnostic quickfix list" })
+vim.keymap.set("n", "<leader>x", vim.diagnostic.setloclist, { desc = "Open [x] diagnostic quickfix list" })
 
 -- Highlight when yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -95,6 +95,27 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.pack.add({ 'https://github.com/tpope/vim-fugitive' })
 
 vim.keymap.set("n", "<leader>g", "<cmd>G<CR>", { desc = "Search [b]uffer" })
+
+-- -----------------------------------------------------------------------------
+-- Diff review (diffview.nvim)
+-- -----------------------------------------------------------------------------
+
+vim.pack.add({ 'https://github.com/sindrets/diffview.nvim' })
+
+-- ponytail: assumes the default branch (origin/HEAD) is the diff base; retype
+-- the ref manually when comparing against something else.
+local function default_branch()
+  local ref = vim.fn.system('git symbolic-ref refs/remotes/origin/HEAD --short 2>/dev/null'):gsub('%s+$', '')
+  return ref ~= '' and ref or 'origin/main'
+end
+
+vim.keymap.set("n", "<leader>do", function() vim.cmd('DiffviewOpen ' .. default_branch() .. '...HEAD') end,
+  { desc = "[d]iffview diff (squashed)" })
+vim.keymap.set("n", "<leader>dh", function()
+  vim.cmd('DiffviewFileHistory --range=' .. default_branch() .. '...HEAD --right-only --no-merges')
+end, { desc = "[d]iffview commit-by-commit" })
+vim.keymap.set("n", "<leader>dl", "<cmd>DiffviewFileHistory %<CR>", { desc = "[d]iffview current file history" })
+vim.keymap.set("n", "<leader>dq", "<cmd>DiffviewClose<CR>", { desc = "[d]iffview close" })
 
 -- -----------------------------------------------------------------------------
 -- Tmux integration (vim-tmux-navigator)
